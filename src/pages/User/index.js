fa5dfd8753f997f3cec9143388f7e7b8028e21ac
Matheus Filipe <mfcr031@gmail.com>
2020-02-14 14:18:51 -0300

    buscando dados da API

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-export default function User(props) {
-  const { route } = props;
+export default class User extends Component {
+  static propTypes = {
+    route: PropTypes.shape({
+      params: PropTypes.shape({
+        user: PropTypes.shape(),
+      }),
+    }).isRequired,
+  };
 
-  const { user } = route.params;
+  state = {
+    stars: [],
+  };
 
-  console.tron.log(user);
+  async componentDidMount() {
+    console.tron.log(this.props);
+    const { route } = this.props;
 
-  return <View />;
+    const { user } = route.params;
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    const { stars } = this.state;
+
+    return <View />;
+  }
 }
